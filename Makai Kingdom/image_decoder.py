35d540fb7d741bc399b69e7760148f83774cff71
Michael Kiss <chainx@hotmail.co.uk>
2024-06-08 13:46:40 +0100

    Updated launch.py to get running on Windows PC with GTX 3070

--- Makai Kingdom/image_decoder.py
+++ Makai Kingdom/image_decoder.py
@@ -14,8 +14,10 @@ text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
 with torch.no_grad():
     image_features = model.encode_image(image)
     text_features = model.encode_text(text)
-    
-    logits_per_image, logits_per_text = model(image, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
 
-print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
+    print(text_features)
+    
+#     logits_per_image, logits_per_text = model(image, text)
+#     probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+# print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
